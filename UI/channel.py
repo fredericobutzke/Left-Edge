fd0cdc6fc94284dd6e4088e0b8a52e5b338e40d1
FredericoButzke <butzkefrederico@gmail.com>
2014-05-06 21:43:39 -0600

    UI: Pins are placed.

--- UI/channel.py
+++ UI/channel.py
@@ -1,13 +1,16 @@
 import Tkinter 
 from Tkinter import *
 
+top_pins = ['0','A','D','E','A','F','G','0','D','I','J','J']
+bottom_pins = ['B','C','E','C','E','B','F','H','I','H','G','I']
+tracks = [['A', 'J'], ['D'], ['E', 'G'], ['C', 'F', 'I'], ['B', 'H']]
+
 top = Tkinter.Tk()
 
-tracks = 5
-track_heigh = 4
-track_space = 2
+track_heigh = 50
+track_space = 25
 
-pins = 5
+pins = len(top_pins)
 pin_width = 100
 pin_space = 50
 
@@ -15,19 +18,30 @@ w = pins * pin_width + pins * pin_space * 2
 h = tracks * track_heigh * track_space * 2
 
 w = Canvas(top)
+w.config(width=150+50*len(top_pins)*5, height=205+len(tracks)*100)
 w.pack()
 
-#Create a pin
-bbox = (0, 0, 100, 100)
-sq = w.create_rectangle(bbox, fill="light pink", tags=("pin", "A"))
-x1, y1, x2, y2 = w.coords(sq)
-bbox = (25, 25, 75, 75)
-sq = w.create_rectangle(bbox, fill="black", tags=("pin", "A"))
-tx = w.create_text((100, 100), text="A", width=25, fill="white")
-items = w.find_withtag("A")
+for j, p in enumerate(top_pins) :
+	if p is not '0' :
+		xplace = j*150+50
+		bbox = (xplace, 5, xplace+100, 105)
+		q = w.create_rectangle(bbox, fill="white", tags=("r1", "pin", p, j))
+		bbox = (xplace+25, 5+25, xplace+100-25, 105-25)
+		sq = w.create_rectangle(bbox, fill="black", tags=("r2", "pin", p, j))
+		tx = w.create_text((xplace+50, 5+50), text=p, width=25, fill="white", tags=("text", "pin", p, j))
 
-for i in items :
-	w.move(i, 50, 50)
+bottom_y = 105+100*len(tracks)
+for j, p in enumerate(bottom_pins) :
+	if p is not '0' :
+		xplace = j*150+50
+		bbox = (xplace, bottom_y, xplace+100, bottom_y+pin_width)
+		q = w.create_rectangle(bbox, fill="white", tags=("r1", "pin", p, j))
+		bbox = (xplace+25, bottom_y+25, xplace+100-25, bottom_y+pin_width-25)
+		sq = w.create_rectangle(bbox, fill="black", tags=("r2", "pin", p, j))
+		tx = w.create_text((xplace+50, bottom_y+50), text=p, width=25, fill="white", tags=("text", "pin", p, j))
+
+# DRAW FIVE TRACKS
 
 mainloop()
 
+# x1, y1, x2, y2 = w.coords(sq)

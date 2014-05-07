b82e9ec8c2983ad0a7a066f5aa7f83924d4e8172
FredericoButzke <butzkefrederico@gmail.com>
2014-05-07 01:41:37 -0600

    UI: Routing and Connecting OK

--- UI/channel.py
+++ UI/channel.py
@@ -10,38 +10,100 @@ top = Tkinter.Tk()
 track_heigh = 50
 track_space = 25
 
-pins = len(top_pins)
 pin_width = 100
 pin_space = 50
 
-w = pins * pin_width + pins * pin_space * 2
-h = tracks * track_heigh * track_space * 2
-
-w = Canvas(top)
-w.config(width=150+50*len(top_pins)*5, height=205+len(tracks)*100)
-w.pack()
+frame=Frame(top)
+frame.grid(row=0,column=0)
+w=Canvas(frame,bg='#FFFFFF',width=1000,height=600,scrollregion=(0,0,5000,5000))
+hbar=Scrollbar(frame,orient=HORIZONTAL)
+hbar.pack(side=BOTTOM,fill=X)
+hbar.config(command=w.xview)
+vbar=Scrollbar(frame,orient=VERTICAL)
+vbar.pack(side=RIGHT,fill=Y)
+vbar.config(command=w.yview)
+w.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+w.pack(side=LEFT,expand=TRUE,fill=BOTH)
 
 for j, p in enumerate(top_pins) :
 	if p is not '0' :
+		print p
 		xplace = j*150+50
 		bbox = (xplace, 5, xplace+100, 105)
-		q = w.create_rectangle(bbox, fill="white", tags=("r1", "pin", p, j))
+		q = w.create_rectangle(bbox, fill="white", tags=('pino', p))
 		bbox = (xplace+25, 5+25, xplace+100-25, 105-25)
-		sq = w.create_rectangle(bbox, fill="black", tags=("r2", "pin", p, j))
-		tx = w.create_text((xplace+50, 5+50), text=p, width=25, fill="white", tags=("text", "pin", p, j))
+		sq = w.create_rectangle(bbox, fill="black", tags=("r2", "pin", "top", p, j))
+		tx = w.create_text((xplace+50, 5+50), text=p, width=25, fill="white", tags=("text", "pin", "top", p, j))
 
-bottom_y = 105+100*len(tracks)
+bottom_y = 75*len(tracks)+125
 for j, p in enumerate(bottom_pins) :
 	if p is not '0' :
 		xplace = j*150+50
 		bbox = (xplace, bottom_y, xplace+100, bottom_y+pin_width)
-		q = w.create_rectangle(bbox, fill="white", tags=("r1", "pin", p, j))
+		q = w.create_rectangle(bbox, fill="white", tags=("r1", "pin","bottom",  p, j))
 		bbox = (xplace+25, bottom_y+25, xplace+100-25, bottom_y+pin_width-25)
-		sq = w.create_rectangle(bbox, fill="black", tags=("r2", "pin", p, j))
-		tx = w.create_text((xplace+50, bottom_y+50), text=p, width=25, fill="white", tags=("text", "pin", p, j))
+		sq = w.create_rectangle(bbox, fill="black", tags=("r2", "pin", "bottom", p, j))
+		tx = w.create_text((xplace+50, bottom_y+50), text=p, width=25, fill="white", tags=("text", "pin", "bottom", p, j))
 
-# DRAW FIVE TRACKS
 
-mainloop()
+# for i in range(5) :
+	# 
+	# bbox = (50, yplace, 300, yplace+track_heigh)
+	# q = w.create_rectangle(bbox, fill="light blue", outline="light blue", tags=("track", i))
 
 # x1, y1, x2, y2 = w.coords(sq)
+
+for i, pins in enumerate(tracks) :
+
+	yplace = i*75+125
+
+	for n in pins :
+
+		# find first and last
+		rtop = top_pins[:]
+		rtop.reverse()
+		rbottom = bottom_pins[:]
+		rbottom.reverse()
+		
+		init = len(top_pins)
+		end = 0
+		if n in top_pins :
+			init = top_pins.index(n)
+			end = len(top_pins)-rtop.index(n)-1
+
+		if n in bottom_pins :
+			foo = bottom_pins.index(n)
+			init = foo if init > foo else init
+			foo = len(bottom_pins)-rbottom.index(n)-1
+			end = foo if end < foo else end
+		
+		# draw the rectangle between them
+		bbox = (150*init+75, yplace, 150*end+125, yplace+track_heigh)
+		tr = w.create_rectangle(bbox, fill="light blue", outline="light blue", tags=("track", n, i))
+
+		# for each pin of that netlist
+		ids = tuple(set(w.find_withtag('top'))&set(w.find_withtag(n))&set(w.find_withtag('r2')))
+		for pn in ids :
+			# 	draw rectangle from its center to the track
+			px1, py1, px2, py2 = w.coords(pn)
+			qx1, qy1, qx2, qy2 = w.coords(tr)
+			bbox = (px1, py2, px1+50, qy2)
+			q = w.create_rectangle(bbox, fill="light pink", outline="light pink", tags=("track", "HORIZONTAL", n, i))
+			bbox = (px1, qy1, px1+50, qy2)
+			q = w.create_rectangle(bbox, fill="light blue", outline="light pink")
+			bbox = (px1+10, qy1+10, px1+40, qy2-10)
+			q = w.create_rectangle(bbox, fill="light pink", outline="light blue")
+		#for each pin of that netlist
+		ids = tuple(set(w.find_withtag('bottom'))&set(w.find_withtag(n))&set(w.find_withtag('r2')))
+		for pn in ids :
+			# 	draw rectangle from its center to the track
+			px1, py1, px2, py2 = w.coords(pn)
+			qx1, qy1, qx2, qy2 = w.coords(tr)
+			bbox = (px1, qy2, px1+50, py1)
+			q = w.create_rectangle(bbox, fill="light pink", outline="light pink", tags=("track", "HORIZONTAL", n, i))
+			bbox = (px1, qy1, px1+50, qy2)
+			q = w.create_rectangle(bbox, fill="light blue", outline="light pink")
+			bbox = (px1+10, qy1+10, px1+40, qy2-10)
+			q = w.create_rectangle(bbox, fill="light pink", outline="light blue")
+
+top.mainloop()
